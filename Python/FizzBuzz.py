ff671d710bf2608a97be7afd17fbe42160ddc806
Pedro Bispo <pedrobispo98pt@gmail.com>
2024-08-16 05:49:16 +0100

    Removed a useless comment and corrected some errors on the top comments

--- Python/FizzBuzz.py
+++ Python/FizzBuzz.py
@@ -1,5 +1,5 @@
 # FizzBuzz Problem
-# Objective: In a range of numbers, for the multiples of 3 print Fizz and for multiple of 5 print Buzz, if the number is both a multiple of 3 and 5 print FizzBuzz
+# Objective: In a range of numbers, for the all multiples of 3 print Fizz and for all multiples of 5 print Buzz, if a number is both a multiple of 3 and 5 print FizzBuzz
 # Formula for multiples: multiple = K * num (K is a constent)
 # For solve this we need to check if the number is dividable by the multiple with a modulus (%) of 0, which is the remainder of the division. 
 # Examples: 
@@ -17,7 +17,6 @@ buzz = []
 fizzbuzz=[]
 out_numbers = []
 
-# For loop, this will calculate the multiples of 2 in range of 100 numbers, starting in 0, add them to an array and print 
 for num in range(1, num_range):
     if(num % k3 == 0 and num % k5 != 0):
         fizz.append(num)
