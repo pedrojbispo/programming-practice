916447d81ce73d911a6095dac81a03c1942c2051
Pedro Bispo <55553435+pedrojbispo@users.noreply.github.com>
2025-02-15 22:54:41 +0000

    Init C++ exercises

---
+++ C++/hello_world.cpp
@@ -0,0 +1,9 @@
+// Just a hello world print :3
+// Nothing special nor new to the world but hey there is always a starting point
+// Quick note, unlike Python C++ requires semicolons at the end of each line
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+}
---
+++ C++/variable_playground.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // In C++ variables must be specified, unlike Python.
+    int a = 0;
+    int b = 80;
+    int c = 70;
+    float x = 3.14;
+    float y = 20.166;
+
+    // Calculation between numbers using vars
+    cout << b + c ;
+    cout << "\n"; // New Line
+    cout << a * b ; // Lmao XD obviously.... it's 0 at the end of the day. 
+    cout << "\n";
+    cout << x / y ;
+    cout << "\n";
+
+    string person1 = "Joe";
+    string person2 = "Anah";
+    // Using multiple vars in a single print.
+    cout << person2 + " talked with " + person1 + "\n";
+
+    int i = 24;
+    float j = 67.143;
+    float result = i - y;
+    cout << result;
+}
