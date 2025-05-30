0265ca96071316a91de1d398618b808799beacba
Pedro Bispo <pedrobispo98pt@gmail.com>
2025-05-30 18:48:37 +0100

    Added FizzBuzz Problem exercises and Fibonacci sequence

---
+++ Java/Fibonacci_Solutions/fibonacci_print.java
@@ -0,0 +1,23 @@
+package Fibonacci_Solutions;
+import java.util.ArrayList;
+
+public class fibonacci_print {
+    public static void main(String[] args){
+        ArrayList<Integer> fib = new ArrayList<>();
+        int fib_num = 0;
+        for (int i=0;i<30;i++){
+            if (i == 0){
+                fib.add(0);
+            }
+            if (i == 1){
+                fib.add(1);
+            }
+            if (i >= 2){
+                fib_num = fib.get(i-1) + fib.get(i-2);
+                fib.add(fib_num);
+            }
+
+        }
+        System.out.println(fib);
+    }
+}
---
+++ Java/Fizz_Buzz_Solutions/fizz_buzz_array.java
@@ -0,0 +1,34 @@
+package Fizz_Buzz_Solutions;
+import java.util.ArrayList;
+public class fizz_buzz_array {
+    public static void main(String[] args){
+        ArrayList<Integer> fizz = new ArrayList<Integer>();
+        ArrayList <Integer> buzz = new ArrayList<Integer>();
+        ArrayList <Integer> fizzbuzz = new ArrayList<Integer>();
+        ArrayList <Integer> outnumbers = new ArrayList<Integer>();
+        int i;
+
+        for (i=1; i<50; i++){
+            if (i % 3 == 0 & i % 5 != 0){
+                fizz.add(i);
+                // System.out.println("Fizz");
+            }
+            if (i % 5 == 0 & i % 3 != 0){
+                buzz.add(i);
+                // System.out.println("Buzz");
+            }
+            if (i % 3 == 0 & i % 5 == 0){
+                fizzbuzz.add(i);
+                // System.out.println("FizzBuzz");
+            }
+            if (i % 3 != 0 & i % 5 != 0){
+                outnumbers.add(i);
+                // System.out.println(i);
+            }
+        }
+        System.out.println("Fizz Numbers: " + fizz);
+        System.out.println("Buzz Numbers: " + buzz);
+        System.out.println("FizzBuzz Numbers: " + fizzbuzz);
+        System.out.println("Out Numbers: " + outnumbers);
+    }
+}
